c7926e8674ed57c86b198bae9d5b813a17981992
Mart836 <muzzom1@gmail.com>
2025-10-21 19:34:45 +0200

    modified:   src/components/Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,7 +30,7 @@ const Login = ({ onLogin }) => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Unified Traffic Management System
+            Traffic Management System
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
